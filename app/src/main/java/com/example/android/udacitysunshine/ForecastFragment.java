1704681ec4c0cd3b1eaf2c43dbd55fdef2128e1c
robinsuri <robin.suri@gmail.com>
2015-03-16 16:46:42 +0530

    start new activity

--- app/src/main/java/com/example/android/udacitysunshine/ForecastFragment.java
+++ app/src/main/java/com/example/android/udacitysunshine/ForecastFragment.java
@@ -1,5 +1,7 @@
 package com.example.android.udacitysunshine;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 
@@ -81,7 +85,20 @@ public class ForecastFragment extends Fragment {
         forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weatherDataList);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(forecastAdapter);
+         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+             String forecast = forecastAdapter.getItem(position);
+                 Toast.makeText(getActivity(),forecast,Toast.LENGTH_SHORT).show();
 
+                 Intent intent = new Intent(getActivity(),DetailActivity.class).putExtra(Intent.EXTRA_TEXT,forecast);
+                 startActivity(intent);
+
+
+
+
+             }
+         });
         new FetchWeatherTask().execute("94043");
 
         return rootView;
